95f358c5e337ae8bcec6104049eae959091ff754
Éric Nascimento <nascimento3ric@gmail.com>
2023-02-17 15:14:44 -0300

    Novo header responsivo

--- src/script.js
+++ src/script.js
@@ -1,70 +1,58 @@
-function enableHorizontalScroll() {
-    var container = document.querySelector('.box-scroll');
-    
-  
-    var isDown = false;
-    var startX;
-    var scrollLeft;
-  
-    container.addEventListener('mousedown', function(e) {
-      isDown = true;
-      startX = e.pageX - container.offsetLeft;
-      scrollLeft = container.scrollLeft;
-      container.style.cursor = 'grabbing';
-    });
-  
-    container.addEventListener('mouseleave', function() {
-      isDown = false;
-      container.style.cursor = 'grab';
-    });
-  
-    container.addEventListener('mouseup', function() {
-      isDown = false;
-      container.style.cursor = 'grab';
-    });
-  
-    container.addEventListener('mousemove', function(e) {
-      if (!isDown) return;
-      e.preventDefault();
-      var x = e.pageX - container.offsetLeft;
-      var walk = (x - startX) * 1.5; // velocidade de rolagem
-      container.scrollLeft = scrollLeft - walk;
-    });
+function focus(){
+    document.getElementById("inputSearchModal").focus();
 }
 
-function enableHorizontalScrollNews() {
-    var container = document.querySelector('.box-scroll-news');
+function enableHorizontalScroll() {
+  var containers = document.querySelectorAll('.box-scroll');
+    // var container = document.getElementsByClassName('.box-scroll');
+   
+    for (let i = 0; i < containers.length; i++) {
+      let container = containers[i];
     
+      var isDown = false;
+      var startX;
+      var scrollLeft;
+      
+      container.addEventListener('mousedown', function(e) {
+        e.preventDefault();
+        isDown = true;
+        startX = e.pageX - container.offsetLeft;
+        scrollLeft = container.scrollLeft;
+        container.style.cursor = 'grabbing';
+        container.style.userSelect = 'auto';
+      });
+    
+      container.addEventListener('mouseleave', function() {
+        isDown = false;
+        container.style.cursor = 'grab';
+      });
+    
+      container.addEventListener('mouseup', function() {
+        // isDown = false;
+        // container.style.cursor = 'grab';
+        // // container.style.cursor = 'pointer';
+        // container.style.userSelect = 'auto';
+
+        isDown = false;
+        container.style.cursor = 'grab';
+        container.style.userSelect = 'auto';
+      });
+    
+      container.addEventListener('mousemove', function(e) {
+        if (!isDown) return;
+        e.preventDefault();
+        var x = e.pageX - container.offsetLeft;
+        var walk = (x - startX) * 1.5; // velocidade de rolagem
+        container.scrollLeft = scrollLeft - walk;
+      });
+
+
+    }
   
-    var isDown = false;
-    var startX;
-    var scrollLeft;
-  
-    container.addEventListener('mousedown', function(e) {
-      isDown = true;
-      startX = e.pageX - container.offsetLeft;
-      scrollLeft = container.scrollLeft;
-      container.style.cursor = 'grabbing';
-    });
-  
-    container.addEventListener('mouseleave', function() {
-      isDown = false;
-      container.style.cursor = 'grab';
-    });
-  
-    container.addEventListener('mouseup', function() {
-      isDown = false;
-      container.style.cursor = 'grab';
-    });
-  
-    container.addEventListener('mousemove', function(e) {
-      if (!isDown) return;
-      e.preventDefault();
-      var x = e.pageX - container.offsetLeft;
-      var walk = (x - startX) * 1.5; // velocidade de rolagem
-      container.scrollLeft = scrollLeft - walk;
-    });
 }
+
+
 
 enableHorizontalScroll();
-enableHorizontalScrollNews();
+
+
